9b6e5f5670bda0cdf7224b866da1754c27c435a3
ɴᴧʀᴜᴛᴏ ᴜᴢᴜᴍᴀᴋɪ <111524889+phoenix-monarch@users.noreply.github.com>
2023-03-11 13:45:38 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ module.exports = withPWA({
     disable: process.env.NODE_ENV === "development",
   },
   images: {
-    domains: ["gogocdn.net"],
+    domains: ["gogoanimes.fi"],
   },
   eslint: {
     ignoreDuringBuilds: true,
